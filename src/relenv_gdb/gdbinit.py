3f7b955c4ef72e6326eea0db6c327faefff21502
Daniel A. Wozniak <dwozniak@vmware.com>
2023-10-22 20:52:46 -0700

    Fix gdb entrypoint

--- src/relenv_gdb/gdbinit.py
+++ src/relenv_gdb/gdbinit.py
@@ -1,13 +1,24 @@
 import pathlib
 import subprocess
 import sys
+import os
 
-real_gdb_bin = pathlib.Path(__file__) / "gdb", "bin", "gdb"
+real_gdb_bin = pathlib.Path(__file__).parent / "gdb"/ "bin"/ "gdb"
+data_directory = pathlib.Path(__file__).parent / "gdb"/ "share" /"gdb"
 
 def main():
     if hasattr(sys, "RELENV"):
-        os.environ["PYTHON_HOME"] = f"{sys.RELENV}"
-        subprocess.popen([str(real_gdb_bin)])
+        os.environ["PYTHONHOME"] = f"{sys.RELENV}"
+        if os.execve in os.supports_fd:
+            with open(real_gdb_bin, "rb") as fp:
+                sys.stdout.flush()
+                sys.stderr.flush()
+                args = [sys.argv[0], f"--data-directory={data_directory}"] + sys.argv[1:]
+                os.execve(fp.fileno(), args, os.environ)
+        else:
+            cmd = real_gdb_bin
+            args = [real_gdb_bin, f"--data-directory={data_directory}"] + sys.argv[1:]
+            os.execve(cmd, args, os.environ)
     else:
         sys.stderr.write(
             "Not running in a relenv environment."
