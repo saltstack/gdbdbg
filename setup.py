76da07310f2234eeea11584c319fa157cd57bc8e
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-09-02 17:30:55 -0700

    Fix python version and platform name

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 # Copyright 2023 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 #
+import platform
+
 from setuptools import Distribution, setup
 
 
-class BinaryDistribution(Distribution):
-    def has_ext_modules(self):
-        return True
+GLIBC_VERSION = "2.17"
 
+
+def plat_name():
+    return f"manylinux_{GLIBC_VERSION.replace('.', '_')}_{platform.machine()}"
 
 setup(
-    distclass=BinaryDistribution,
+    options={
+        "bdist_wheel": {
+            "plat_name": f"{plat_name()}",
+            "python_tag": "py3",
+        }
+    }
 )
