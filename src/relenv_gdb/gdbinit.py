0fa0c527f4efcc4400af168433da14ad2688c80f
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-09-05 23:28:55 -0700

    Work on non relenv pythons

--- src/relenv_gdb/gdbinit.py
+++ src/relenv_gdb/gdbinit.py
@@ -14,21 +14,16 @@ data_directory = pathlib.Path(__file__).parent / "gdb" / "share" / "gdb"
 
 def main():
     """Wrap gdb startup."""
-    if hasattr(sys, "RELENV"):
-        os.environ["PYTHONHOME"] = f"{sys.RELENV}"
-        if os.execve in os.supports_fd:
-            with open(real_gdb_bin, "rb") as fp:
-                sys.stdout.flush()
-                sys.stderr.flush()
-                args = [sys.argv[0], f"--data-directory={data_directory}"] + sys.argv[
-                    1:
-                ]
-                os.execve(fp.fileno(), args, os.environ)
-        else:
-            cmd = real_gdb_bin
-            args = [real_gdb_bin, f"--data-directory={data_directory}"] + sys.argv[1:]
-            os.execve(cmd, args, os.environ)
+    os.environ["PYTHONPATH"] = os.pathsep.join(sys.path)
+    if os.execve in os.supports_fd:
+        with open(real_gdb_bin, "rb") as fp:
+            sys.stdout.flush()
+            sys.stderr.flush()
+            args = [sys.argv[0], f"--data-directory={data_directory}"] + sys.argv[
+                1:
+            ]
+            os.execve(fp.fileno(), args, os.environ)
     else:
-        sys.stderr.write("Not running in a relenv environment.")
-        sys.stderr.flush()
-        sys.exit(1)
+        cmd = real_gdb_bin
+        args = [real_gdb_bin, f"--data-directory={data_directory}"] + sys.argv[1:]
+        os.execve(cmd, args, os.environ)
